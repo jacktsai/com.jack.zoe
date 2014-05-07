ee09a9cc13eb1bd5f5a65979cdec0556fcd59589
Jack Tsai <wa0327@gmail.com>
2014-05-07 22:12:07 +0800

    use ViewPager and PagerAdapter to implement image flipping

--- zoe/src/main/java/com/jack/zoe/MainActivity.java
+++ zoe/src/main/java/com/jack/zoe/MainActivity.java
@@ -1,13 +1,21 @@
 package com.jack.zoe;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 import java.util.Timer;
@@ -15,100 +23,120 @@ import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 
-    private TimerTask pictureRollingTask;
+    private static String TAG = "Zoe";
+
+    private int[] picArray = new int[] { R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j };
+    private int currentImageIndex = 0;
+    private Timer imageScrollTimer;
+    private TimerTask imageScrollTask;
+    private ViewPager imagePager;
     private MessageAnimation messageAnimator;
     private MediaPlayer mp3Player;
-
-    private GestureDetector gd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         super.setContentView(R.layout.activity_main);
 
-        this.startRollingPicture();
-
         this.messageAnimator = new MessageAnimation(this);
         this.messageAnimator.start();
 
-//        this.startBGM();
+        this.startBGM();
 
-
-        this.gd = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+        PicturesAdapter adapter = new PicturesAdapter();
+        this.imagePager = (ViewPager)super.findViewById(R.id.pictures);
+        this.imagePager.setAdapter(adapter);
+        this.imagePager.setOffscreenPageLimit(adapter.getCount());
+        this.imagePager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
-            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-                if ( Math.abs(velocityX) > 200)
-                {
-                    float x1 = e1.getX(), x2 = e2.getX();
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
 
-                    if (Math.abs(x1 - x2) > 120) {
-                        if ( x1 > x2 ) {
-                            Log.e("Main", "Swipe from right to left.");
-                        } else {
-                            Log.e("Main", "Swipe from left to right.");
-                        }
+            @Override
+            public void onPageSelected(int position) {
+                Log.e(TAG, String.format("onPageSelected %d", position));
+                currentImageIndex = position;
+                stopScrollImage();
+                startScrollImage();
+            }
 
-                        return true;
-                    }
-                }
-
-                return super.onFling(e1, e2, velocityX, velocityY);
+            @Override
+            public void onPageScrollStateChanged(int state) {
             }
         });
+
+        this.startScrollImage();
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return this.gd.onTouchEvent(event);
+    class PicturesAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return picArray.length;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+            ImageView view = new ImageView(MainActivity.this);
+            view.setImageResource(picArray[position]);
+            view.setMaxHeight(300);
+            view.setBackgroundColor(Color.DKGRAY);
+
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View)object);
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+    }
+
+    class ImageScrollTask extends TimerTask {
+        private Runnable runnable = new Runnable() {
+            public void run() {
+                int index = currentImageIndex;
+                index = index + 1;
+                if ( index >= picArray.length) {
+                    index = 0;
+                }
+                imagePager.setCurrentItem(index);
+            }
+        };
+
+        @Override
+        public void run() {
+            MainActivity.this.runOnUiThread(runnable);
+        }
     }
 
     @Override
     protected void onDestroy() {
-//        this.stopBGM();
+        this.stopBGM();
         this.messageAnimator.cancel();
-        this.stopRollingPicture();
+        this.stopScrollImage();
         super.onDestroy();
     }
 
-    private void startRollingPicture() {
-        this.pictureRollingTask = new TimerTask() {
-            private int[] picArray = new int[] { R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f, R.drawable.g, R.drawable.h, R.drawable.i, R.drawable.j };
-            private int currentIndex = 0;
-            private ImageView zoePicture = (ImageView)MainActivity.this.findViewById(R.id.zoePicture);
-
-            private Runnable runnable = new Runnable() {
-                public void run() {
-                    displayPicture();
-                    prepareNextPicture();
-                }
-
-                private void displayPicture() {
-                    zoePicture.setImageResource(picArray[currentIndex]);
-                }
-
-                private void prepareNextPicture() {
-                    currentIndex = currentIndex + 1;
-                    if ( currentIndex >= picArray.length) {
-                        currentIndex = 0;
-                    }
-                }
-            };
-
-            @Override
-            public void run() {
-                MainActivity.this.runOnUiThread(runnable);
-            }
-        };
-
-        new Timer().schedule(pictureRollingTask, 100, 3000);
+    private void startScrollImage() {
+        this.imageScrollTask = new ImageScrollTask();
+        this.imageScrollTimer = new Timer();
+        this.imageScrollTimer.schedule(this.imageScrollTask, 5000);
     }
 
-    private void stopRollingPicture() {
-        this.pictureRollingTask.cancel();
+    private void stopScrollImage() {
+        this.imageScrollTimer.cancel();
+        this.imageScrollTask.cancel();
     }
 
     private void startBGM() {
-        int[] mp3Array = new int[] { R.raw.dance_of_the_dragonfly/*, R.raw.sundial_reams, R.raw.through_the_arbor*/ };
+        int[] mp3Array = new int[] { R.raw.dance_of_the_dragonfly, R.raw.sundial_reams, R.raw.through_the_arbor };
         Random r = new Random();
         int selectedMp3 = mp3Array[r.nextInt(3)];
 
