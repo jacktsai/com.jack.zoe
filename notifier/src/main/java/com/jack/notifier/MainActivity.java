7a85ec8c70bd5a9ef0020b2a02f40ffa16df1b6f
Jack Tsai <wa0327@gmail.com>
2014-05-09 11:21:12 +0800

    adjust notifier's dependencies

--- notifier/src/main/java/com/jack/notifier/MainActivity.java
+++ notifier/src/main/java/com/jack/notifier/MainActivity.java
@@ -15,8 +15,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        super.setContentView(R.layout.activity_main);
-        super.setContentView(com.android.internal.R.layout.notification_template_base);
+        super.setContentView(R.layout.activity_main);
 
 //        Button createNotification = (Button)super.findViewById(R.id.createNotification);
 //        createNotification.setOnClickListener(new View.OnClickListener() {
